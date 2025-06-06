3857c0b463294d3a7a9e2bc7bd76bf25766d76c6
RajuLopchan <139037780+RajuLopchan@users.noreply.github.com>
2025-06-06 15:25:24 +0545

    context api eg

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { createContext, useContext } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const UserContext = createContext();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function GrandChild() {
+  // 3. Consume context here
+  const user = useContext(UserContext);
+  return <p>User name is: {user.name}</p>;
 }
 
-export default App
+function Child() {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <GrandChild />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <UserContext.Provider value={{ name: "Raju" }}>
+      <div>
+        <h1>Context API Example</h1>
+        <Child />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
