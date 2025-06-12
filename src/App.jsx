1a75cad31df590c85d92cd95d2ee4b163edd6551
RajuLopchan <139037780+RajuLopchan@users.noreply.github.com>
2025-06-12 14:14:35 +0545

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      {/* <UseEffect /> */}
-      {/* <UseCallback /> */}
-      {/* <UseMemo /> */}
-      {/* <UseRef /> */}
-      {/* <UseState /> */}
-      <Operation />
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
